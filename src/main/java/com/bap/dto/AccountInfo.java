4741c24ef333fa71451d1932f5e102af96bccfe8
Habay <biodun.banjoko@yahoo.com>
2024-01-18 10:12:18 +0100

    dto class for users

---
+++ src/main/java/com/bap/dto/AccountInfo.java
@@ -0,0 +1,19 @@
+package com.bap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountInfo {
+
+	private String accountName;
+	
+	private String accountNumber;
+	
+	private String accountBalance;
+}
