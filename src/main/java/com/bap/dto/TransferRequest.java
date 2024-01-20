d2adeded39bc60b2fd79058acfa91e3d9cf0a5bc
Habay <biodun.banjoko@yahoo.com>
2024-01-20 12:51:18 +0100

    transfer request wip

---
+++ src/main/java/com/bap/dto/TransferRequest.java
@@ -0,0 +1,19 @@
+package com.bap.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransferRequest {
+
+	private String sourceAccountNumber;
+	private String destinationAccountNumber;
+	private Double amount;
+}
