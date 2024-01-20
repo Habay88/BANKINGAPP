1ba9576975dd5844021d68b2c5e726c1b49ed5c0
Habay <biodun.banjoko@yahoo.com>
2024-01-20 22:39:24 +0100

    open api documentation

--- src/main/java/com/bap/BankingappApplication.java
+++ src/main/java/com/bap/BankingappApplication.java
@@ -3,7 +3,33 @@ package com.bap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title= "THE BANK APPLICATION API ",
+		description = "Backend Rest API  for BANK APP",
+		version="v1.0",
+		contact= @Contact(
+			name = "Banjoko Abiodun Lateef",
+			email = "biodun.banjoko@yahoo.com",
+			url = "https://github.com/Habay88/BANKINGAPP"
+		),
+		license = @License(
+			name = "habay_inc",
+			url="https://github.com/Habay88/BANKINGAPP"
+		)
+),
+externalDocs = @ExternalDocumentation(
+	description = "Bank App Documentation",
+	url = "https://github.com/Habay88/BANKINGAPP"
+)
+)
 public class BankingappApplication {
 
 	public static void main(String[] args) {
