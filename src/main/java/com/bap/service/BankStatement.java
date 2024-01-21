5f720b9b72f508d4a4ecee4e11ad5ed57998a6d5
Habay <biodun.banjoko@yahoo.com>
2024-01-21 20:24:16 +0100

    statement of account wip

---
+++ src/main/java/com/bap/service/BankStatement.java
@@ -0,0 +1,32 @@
+package com.bap.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+
+import org.springframework.stereotype.Component;
+
+import com.bap.entity.TransactionHistory;
+import com.bap.repository.TransactionRepository;
+
+import lombok.AllArgsConstructor;
+
+@Component
+
+@AllArgsConstructor
+public class BankStatement {
+
+	private TransactionRepository transactionRepository;
+	// retreive list of trasactions  within a date range given an account number 
+	// generate a pdf file of transaction 
+	// send the file via email
+	public List<TransactionHistory> generateStatement(String accountNumber, String startDate, String endDate){
+		LocalDate start = LocalDate.parse(startDate,DateTimeFormatter.ISO_DATE);
+		LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
+		List<TransactionHistory> transactionList = transactionRepository.findAll().stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
+		.filter(transaction -> transaction.getCreatedAt().isEqual(start)).filter(transaction -> transaction.getCreatedAt().isEqual(end)).toList();
+		return null;
+
+	}
+}
